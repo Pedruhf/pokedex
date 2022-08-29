3541d6cfff2e37df27440d4b8f1462dd47f00a36
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-08-29 15:03:48 -0300

    feat: improve pokemon card layotu

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -1,14 +1,19 @@
 import { Component, Input } from '@angular/core';
+import { PokemonModel } from "../models";
 
 @Component({
   selector: 'app-pokemon-card',
   templateUrl: './pokemon-card.component.html',
-  styleUrls: ['./pokemon-card.component.sass']
+  styleUrls: ['./pokemon-card.component.scss']
 })
 export class PokemonCardComponent {
   @Input()
-  pokemon!: { name: string };
+  pokemon!: PokemonModel;
 
 
   constructor() { }
+
+  getPokemonImage(): string {
+    return `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${this.pokemon.id}.png`;
+  }
 }
--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonModel } from "../models";
 
 @Component({
   selector: 'pokemon-list',
   templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.sass'],
+  styleUrls: ['./pokemon-list.component.scss'],
 })
 export class PokemonListComponent implements OnInit {
-  pokemonList = [
-    { name: "Charmander" },
-    { name: "Ivysaur" },
-    { name: "Pika" },
+  pokemonList: PokemonModel[] = [
+    { id: "001", name: "Charmander" },
+    { id: "002", name: "Ivysaur" },
+    { id: "003", name: "Pika" },
   ];
   constructor() { }
   
